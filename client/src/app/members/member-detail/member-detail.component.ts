5f56df32ca9f2e93cc147ce8f2fb436c9037ccd3
Tris <tris.magp@gmail.com>
2024-10-21 09:46:57 +0200

    Section 9-105 add member detailed page

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { ActivatedRoute } from '@angular/router';
+import { Member } from '../../_models/member';
+
+
 
 @Component({
   selector: 'app-details',
@@ -8,5 +13,24 @@ import { Component } from '@angular/core';
   styleUrl: './member-detail.component.css'
 })
 export class MemberDetailComponent {
+  private memberService = inject(MembersService);
+  private route = inject(ActivatedRoute);
+  
 
+  member?: Member;
+
+
+  ngOnInit(): void {
+    this.loadMember()
+  }
+
+  loadMember() {
+    const username = this.route.snapshot.paramMap.get('username');
+    if (!username) return;
+    this.memberService.getMember(username).subscribe({
+      next: member => {
+        this.member = member;
+      }
+    })
+  }
 }
