94daf83a2e674659f4af7738ebc7f01eadd1de13
Tris <tris.magp@gmail.com>
2024-10-21 10:08:08 +0200

    End of section 9

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -3,13 +3,13 @@ import { MembersService } from '../../_services/members.service';
 import { ActivatedRoute } from '@angular/router';
 import { Member } from '../../_models/member';
 import { TabsModule } from 'ngx-bootstrap/tabs';
-// import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
+import { GalleryItem, GalleryModule, ImageItem } from 'ng-gallery';
 
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [TabsModule],
+  imports: [TabsModule,GalleryModule],
   templateUrl: './member-detail.component.html',
   styleUrl: './member-detail.component.css'
 })
@@ -18,7 +18,7 @@ export class MemberDetailComponent {
   private route = inject(ActivatedRoute);
   
   member?: Member;
-  // images: GalleryItem[] = [];
+  images: GalleryItem[] = [];
 
 
   ngOnInit(): void {
@@ -31,6 +31,9 @@ export class MemberDetailComponent {
     this.memberService.getMember(username).subscribe({
       next: member => {
         this.member = member;
+        member.photos.map(p => {
+          this.images.push(new ImageItem({src: p.url, thumb: p.url}))
+        })
       }
     })
   }
