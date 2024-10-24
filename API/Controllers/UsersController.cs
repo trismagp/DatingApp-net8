e7f22d72d6a801e4d294f8b7ffa28dd86cc98bf3
Tris <tris.magp@gmail.com>
2024-10-24 16:34:32 +0200

    Section 10-113 persisting the changes in the api

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,11 +4,12 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using AutoMapper;
 using API.Interfaces;
+using System.Security.Claims;
 
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
@@ -24,5 +25,26 @@ public class UsersController(IUserRepository userRepository) : BaseApiController
         var user = await userRepository.GetMemberAsync(username);
         if (user == null) return NotFound(); 
         return user;
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
+    {
+
+        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+        if(username == null ) return BadRequest("no username found in token");
+        var user = await userRepository.GetUserByUsernameAsync(username);
+
+        // var user = await unitOfWork.UserRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        // if (user == null) return BadRequest("Could not find user");
+
+        mapper.Map(memberUpdateDto, user);
+        
+        // if (await unitOfWork.Complete()) return NoContent();
+
+        if (await userRepository.SaveAllAsync()) return NoContent();
+        return BadRequest("Failed to update the user");
     }
 }
