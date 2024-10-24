942d6a5acad4a606a4ec132f428ebd2aca4ced80
Tris <tris.magp@gmail.com>
2024-10-24 08:56:47 +0200

    Section 10-110 create the edit template form

---
+++ client/src/app/members/member-edit/member-edit.component.ts
@@ -0,0 +1,37 @@
+import { Component, inject, OnInit } from '@angular/core';
+import { MembersService } from '../../_services/members.service';
+import { Member } from '../../_models/member';
+import { AccountService } from '../../_services/account.service';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-member-edit',
+  standalone: true,
+  imports: [TabsModule,FormsModule],
+  templateUrl: './member-edit.component.html',
+  styleUrl: './member-edit.component.css'
+})
+export class MemberEditComponent implements OnInit{
+  private memberService = inject(MembersService);
+  private accountService = inject(AccountService);
+  member?: Member;
+  
+
+
+  ngOnInit(): void {
+    this.loadMember()
+  }
+
+  loadMember() {
+    const user = this.accountService.currentUser();
+    if (!user) return;
+    this.memberService.getMember(user.username).subscribe({
+      next: member => {
+        this.member = member;
+      }
+    })
+  }
+
+
+}
