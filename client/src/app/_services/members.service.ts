8a4bd8f400e9b9bfbd2778d2d47f08794f37036b
Tris <tris.magp@gmail.com>
2024-10-25 12:06:38 +0200

    Section 10 end - 116 using service to store state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,8 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject } from '@angular/core';
+import { Injectable, inject, signal } from '@angular/core';
 
 import { Member } from '../_models/member';
 import { environment } from '../../environments/environment';
+import { of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,32 @@ import { environment } from '../../environments/environment';
 export class MembersService {
   private http = inject(HttpClient);
   baseUrl = environment.apiUrl;
+  members = signal<Member[]>([]);
+
+
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');
+    return this.http.get<Member[]>(this.baseUrl + 'users').subscribe({
+      next: members => this.members.set(members)
+    });
+
+
   }
   getMember(username: string) {
+    const member = this.members().find(x=>x.username===username);
+    // ==> of(member) return a member as observable
+    if (member !== undefined) return of(member); 
+    
     return this.http.get<Member>(this.baseUrl + 'users/' + username);
   }
 
   updateMember(member: Member) {
     return this.http.put(this.baseUrl + 'users', member).pipe(
-      // tap(() => {
-      //   this.members.update(members => members.map(m => m.username === member.username 
-      //       ? member : m))
-      // })
+      tap(() => {
+        this.members.update(members => 
+          members.map(m => 
+            m.username === member.username 
+            ? member : m))
+      })
     )
   }
-
- 
 }
